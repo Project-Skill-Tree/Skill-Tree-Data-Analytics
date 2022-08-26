2c78c369940ee2e904b7bc04dbeee23b6ade4c78
Alex Scofield <alexscofieldt@gmail.com>
2022-08-26 20:28:33 +0200

    Added various methods. Some methods are still in construction. Ready to share on github to receive feedback. Will add examples soon.

--- src/play.py
+++ src/play.py
@@ -2,6 +2,3 @@ import stdata
 
 r = stdata.Reader()
 
-print(len(r.get_skill_completion_rate().keys()))
-print(r.list_skills_by_ease(skill_parameter={'category':'MENTAL HEALTH'}))
-r.graph_skills_by_popularity()
--- src/stdata.py
+++ src/stdata.py
@@ -16,6 +16,13 @@ class Reader (object):
         self.skills = self.db.Skills
         self.tasks = self.db.Tasks
 
+    @staticmethod
+    def set_plot(title: str, tight_layout: bool) -> None:
+        plt.style.use("seaborn-darkgrid")
+        plt.title(title)
+        if tight_layout:
+            plt.tight_layout
+        plt.show()
     
     def count_users(self, parameter={}) -> int:
         return len(list(self.users.find(parameter)))
@@ -47,18 +54,13 @@ class Reader (object):
         data = {k: uncut_data[k] for k in list(uncut_data)[:amount]}
         skills = list(data.keys())
         performance = list(data.values())
-        plt.style.use("seaborn-darkgrid")
         plt.barh(skills, performance)
         plt.xlabel('Number of completions')
-        plt.title('Skill Popularity')
 
         for i, v in enumerate(performance):
             plt.text(v + 1, i, str(v), color='blue', fontweight='bold')
         
-        if tight_layout:
-            plt.tight_layout()
-
-        plt.show()
+        self.set_plot("Skill Popularity", tight_layout)
 
     def order_challenges_by_popularity(self, user_parameter={}) -> list:
         from collections import Counter, OrderedDict
@@ -85,18 +87,14 @@ class Reader (object):
         data = {k: uncut_data[k] for k in list(uncut_data)[:amount]}
         challenges = list(data.keys())
         performance = list(data.values())
-        plt.style.use("seaborn-darkgrid")
         plt.barh(challenges, performance)
         plt.xlabel('Number of completions')
-        plt.title('Challenge Popularity')
 
         for i, v in enumerate(performance):
             plt.text(v + 1, i, str(v), color='blue', fontweight='bold')
         
-        if tight_layout:
-            plt.tight_layout()
-
-        plt.show()
+        self.set_plot("Challenge Popularity", tight_layout)
+        
     
     def get_skill_completion_rate(self, user_parameter={}, skill_parameter={}) -> dict:
         from collections import Counter
@@ -120,12 +118,24 @@ class Reader (object):
 
         return data_ordered
     
-    def list_skills_by_ease(self, skill_parameter={}):
+    def list_skills_by_ease(self, skill_parameter={}) -> dict:
         data = self.get_skill_completion_rate(skill_parameter=skill_parameter)
         keys = [self.skills.find_one({"_id":id})["title"] for id in data.keys()]
         values = [value['Score'] for value in data.values()]
         total_dict = dict(zip(keys, values))
         return total_dict
+    
+    def graph_skills_by_ease(self, skill_parameter={}, tight_layout=False) -> None:
+        data = self.list_skills_by_ease(skill_parameter=skill_parameter)
+        plt.bar(data.keys(), data.values())
+        plt.xlabel("Completion_rate")
+        self.set_plot("Skills by ease", tight_layout=tight_layout)
+    
+    def graph_xp_distribution(self, user_parameter={}, tight_layout=True) -> None:
+        xp_list = [user["xp"] for user in self.users.find(user_parameter)]
+        plt.hist(xp_list)
+        plt.yscale("log")
+        self.set_plot("XP Distribution", tight_layout)
 
     def close(self) -> None:
         self.client.close()
